866380427e2171ac675f668e2ced082db27b2a79
Asadullah Hamidi <asad.hamidi119@gmail.com>
2021-10-03 02:10:13 +0530

    Final redesign (hopefully)

--- script.js
+++ script.js
@@ -14,20 +14,35 @@ if (theme.getAttribute("href") == "style.css") {
 })
 
 let dropdown = document.querySelector(".dropdown");
-let links = document.getElementById("link-menu");
-let links_open = false;
+let links_nav = document.querySelector("nav ul");
+let links_1 = false;
 
 dropdown.addEventListener("click", function() {
+    console.log("Check");
+    if (!links_1)
+    {
+        links_1 = true;
+        links_nav.style.display = "none";
+    }
+    else {
+        links_1 = false;
+        links_nav.style.display = "flex";
+    }
+})
+
+let link_btn = document.querySelector(".links-list");
+let links = document.querySelector(".dropdown-links");
+let links_open = false;
+
+link_btn.addEventListener("click", function() {
     console.log("Check");
     if (!links_open)
     {
         links_open = true;
-        links.classList.add("links-reveal");
-        links.classList.remove("links");
+        links.style.display = "flex";
     }
     else {
         links_open = false;
-        links.classList.add("links");
-        links.classList.remove("links-reveal");
+        links.style.display = "none";
     }
 })
