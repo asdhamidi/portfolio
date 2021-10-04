5844ffd0cfec8c747826bfea7998df9bddd5e563
Asadullah Hamidi <asad.hamidi119@gmail.com>
2021-10-04 07:57:10 +0530

    Improved the menu button, page flow for mobile users, made changes in HTML as per Lighthouse audit.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Theme changing part
+
 let btn = document.querySelector(".theme-toggle");
 let theme = document.getElementById("theme-source");
 let btn_icon = document.getElementById("theme-toggle")
@@ -13,10 +15,10 @@ if (theme.getAttribute("href") == "style.css") {
     }
 })
 
+// Dropdown menu for small screens part
 let dropdown = document.querySelector(".dropdown");
 let links_nav = document.querySelector("nav ul");
-let links_1 = false;
-
+let links_1 = true;
 dropdown.addEventListener("click", function() {
     console.log("Check");
     if (!links_1)
@@ -30,6 +32,7 @@ dropdown.addEventListener("click", function() {
     }
 })
 
+// Social Media Menu for smaller screens part
 let link_btn = document.querySelector(".links-list");
 let links = document.querySelector(".dropdown-links");
 let links_open = false;
