07463722001152e4940a320839386aee43923f8c
Asadullah Hamidi <asad.hamidi119@gmail.com>
2021-10-06 13:53:32 +0530

    BG changes, file cleanup, and UI changes.

--- js/script.js
+++ js/script.js
@@ -5,22 +5,21 @@ let theme = document.getElementById("theme-source");
 let btn_icon = document.getElementById("theme-toggle")
 
 btn.addEventListener("click", function () {
-if (theme.getAttribute("href") == "style.css") {
-        theme.href = "dark.css";
+if (theme.getAttribute("href") == "./css/style.css") {
+        theme.href = "./css/dark.css";
         btn_icon.src = "./images/day.svg";
     }
     else {
-        theme.href = "style.css";
+        theme.href = "./css/style.css";
         btn_icon.src = "./images/night.svg";
     }
-})
+});
 
 // Dropdown menu for small screens part
 let dropdown = document.querySelector(".dropdown");
 let links_nav = document.querySelector("nav ul");
 let links_1 = true;
 dropdown.addEventListener("click", function() {
-    console.log("Check");
     if (!links_1)
     {
         links_1 = true;
@@ -30,8 +29,14 @@ dropdown.addEventListener("click", function() {
         links_1 = false;
         links_nav.style.display = "flex";
     }
-})
+});
 
+links_nav.addEventListener("click", function () {
+    if (window.innerWidth <= 855 && !links_1) {
+        links_1 = true;
+        links_nav.style.display = "none";
+    }
+});
 // Social Media Menu for smaller screens part
 let link_btn = document.querySelector(".links-list");
 let links = document.querySelector(".dropdown-links");
@@ -40,6 +45,7 @@ let links_open = false;
 link_btn.addEventListener("click", function() {
     if (!links_open)
     {
+        console.log("in");
         links_open = true;
         links.style.display = "flex";
     }
@@ -47,14 +53,17 @@ link_btn.addEventListener("click", function() {
         links_open = false;
         links.style.display = "none";
     }
-})
+});
 
 let close_btn = document.getElementById("close");
 close_btn.addEventListener("click", function closemenu() {
     links.style.display = "none";
     links_open = false;
 });
+
 document.querySelector("main").addEventListener("click", function closemenu() {
     links.style.display = "none";
     links_open = false;
+    links_nav.style.display = "none";
+    links_1 = true;
 });
